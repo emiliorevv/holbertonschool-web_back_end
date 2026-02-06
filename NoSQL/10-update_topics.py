780bcb59e4955f884ab38dbdffe90d2b35e48e90
Emilio Revueltas <emilio.revueltas@gmail.com>
2026-02-05 18:42:06 -0600

    update

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Módulo para actualizar documentos en una colección de MongoDB.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Cambia todos los temas de un documento de escuela basado en el nombre.
+
+    Args:
+        mongo_collection: Objeto de colección de pymongo.
+        name (string): El nombre de la escuela a buscar.
+        topics (list of strings): La lista de temas a establecer.
+
+    Returns:
+        Nada.
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+
+
+if __name__ == "__main__":
+    # El código no se ejecuta al ser importado
+    pass
