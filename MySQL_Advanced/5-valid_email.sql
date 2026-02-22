c68d2486cee7769b83d3909ed22fb57954735559
Emilio Revueltas <emilio.revueltas@gmail.com>
2026-02-21 20:49:05 -0600

    update

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- Script that creates a trigger to reset valid_email only when the email is changed
+DELIMITER $$
+
+
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END $$
+
+DELIMITER ;
